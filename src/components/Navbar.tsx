a18b7954d9dd9a531b24e7f2fee20ae47bf89ce5
Vipplynn <christianc0000@gmail.com>
2025-05-03 03:39:24 -0700

    added logo to nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,30 @@
+
+import logo from "../assets/logo.png";
+
 function Navbar() {
-    return (
-      <nav className="w-full fixed top-0 left-0 bg-white z-50 shadow">
-        <div className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center">
-          <span className="text-lg font-semibold">Christian Cho</span>
-          <div className="space-x-4 text-sm">
-            <a href="#about" className="hover:underline">About</a>
-            <a href="#featured-projects" className="hover:underline">Projects</a>
-            <a href="#contact" className="hover:underline">Contact</a>
-          </div>
+  return (
+    <nav className="w-full fixed top-0 left-0 bg-white z-50 shadow">
+      <div className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center">
+        {/* Logo + Site Name */}
+        <a href="#hero" className="flex items-center">
+          <img src={logo} alt="Christian Cho Logo" className="h-8 w-8 mr-2" />
+        </a>
+
+        {/* Navigation Links */}
+        <div className="space-x-4 text-sm">
+          <a href="#about" className="hover:underline">
+            About
+          </a>
+          <a href="#featured-projects" className="hover:underline">
+            Projects
+          </a>
+          <a href="#contact" className="hover:underline">
+            Contact
+          </a>
         </div>
-      </nav>
-    );
-  }
-  
-  export default Navbar;
-  
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
